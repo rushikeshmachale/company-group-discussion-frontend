693c74dcbb72a566d83ddd056e87c55c54977250
ShrinathGutte <77496656+ShrinathGutte@users.noreply.github.com>
2023-11-25 15:17:04 +0530

    App file updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import EmployeeDashboard from "./components/employee/EmployeeDashboard";
 import EmployeeChat from "./components/employee/EmployeeChat";
+import Admin from "./components/manager/Admin";
 import Navbar from "./components/navbar/Navbar";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Route path="/" element={<EmployeeDashboard />} />
         <Route path="/edashboard" element={<EmployeeDashboard />} />
         <Route path="/echat" element={<EmployeeChat />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
