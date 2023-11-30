a7921598a6f200ff6b78fb5946fb5bc99c1417c2
RupaliMore04 <rupali4601@gmail.com>
2023-11-30 19:48:46 +0530

    Messageservice updated

--- src/components/CreateGroup.js
+++ src/components/CreateGroup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import MyGroupService from './MyGroupService';
+
 
 const CreateGroup = () => {
     return (
--- src/services/Message.service.js
+++ src/services/Message.service.js
@@ -1,26 +1,77 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8888/api/v1/message";
+const API_URL = "http://localhost:8888/api/v1/messages";
 
 class MessageService {
-   saveMessage(message) {
-        return axios.post(API_URL,message);
-    }
-    
-  getAllMessagesById(id) {
-    return axios.get(API_URL + "/" + id);
+  saveMessage(message) {
+    return axios.post(API_URL, message)
+      .then(response => {
+        
+        console.log('Message saved successfully:', response.data);
+        return response.data; 
+      })
+      .catch(error => {
+       
+        console.error('Error saving message:', error);
+        throw error; 
+      });
   }
 
-  getAllMessagesByGroupId(id){
-    return axios.get(API_URL + "/group/" + id);
+  getAllMessagesById(id) {
+    return axios.get(`${API_URL}/${id}`)
+      .then(response => {
+       
+        console.log('Messages retrieved successfully:', response.data);
+        return response.data; 
+      })
+      .catch(error => {
+        
+        console.error('Error retrieving messages:', error);
+        throw error; 
+      });
+  }
+
+  getMessageById(id) {
+    return axios.get(`${API_URL}/${id}/message`)
+      .then(response => {
+        
+        console.log('Message retrieved successfully:', response.data);
+        return response.data; 
+      })
+      .catch(error => {
+        
+        console.error('Error retrieving message:', error);
+        throw error; 
+      });
   }
 
   deleteMessage(id) {
-    return axios.delete(API_URL +"/"+ id);
+    return axios.delete(`${API_URL}/${id}`)
+      .then(response => {
+       
+        console.log('Message deleted successfully:', response.data);
+        return response.data; 
+      })
+      .catch(error => {
+       
+        console.error('Error deleting message:', error);
+        throw error; 
+      });
   }
 
   editMessage(id, message) {
-    return axios.put(API_URL +"/" +id, message);
+    return axios.put(`${API_URL}/${id}`, message)
+      .then(response => {
+        
+        console.log('Message edited successfully:', response.data);
+        return response.data; 
+      })
+      .catch(error => {
+        
+        console.error('Error editing message:', error);
+        throw error; 
+      });
   }
 }
+
 export default new MessageService();
